a815bda581d5615db995485785e546d6990a93d0
Rui Zhang <rui@dhcp154.merusouth116.iit.edu>
2018-01-06 22:47:38 -0600

    Initial Commit

---
+++ LeetCode200_NumberOfIslands/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode200_NumberOfIslands
+//
+//  Created by Rui on 1/6/18.
+//  Copyright © 2018 Rui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
